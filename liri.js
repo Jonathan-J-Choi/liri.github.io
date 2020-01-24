2323309df877fef876ee8579f17f82bb02f75425
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2020-01-23 19:53:11 -0500

    just starting

--- liri.js
+++ liri.js
@@ -1,4 +1,13 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+let keys = require("./keys.js");
+const axios = require("axios");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+let query = process.argv[2]
+
+axios
+  .get("https://www.omdbapi.com/?t=" +query+ "&apikey=trilogy")
+  .then(function(res) {
+    console.log(res.data);
+  });
+
