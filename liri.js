48d40a7070fb81b3ba78dfd51385ec68de06fbe1
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2020-01-25 14:29:37 -0500

    removed commands.js file and starting to build choice

--- liri.js
+++ liri.js
@@ -1,10 +1,32 @@
 require("dotenv").config();
-let keys = require("./keys.js");
+const keys = require("./keys");
+const fs = require("fs");
+const spotify = new Spotify(keys.spotify);
+const Spotify = require("node-spotify-api");
+const moment = require("moment");
 const axios = require("axios");
 
-// var spotify = new Spotify(keys.spotify);
-let query = process.argv[2]
 
+let query = process.argv[2]
+let userQuery = process.argv.slice(3).join(" ");
+console.log(userQuery)
+
+let choice ={
+  "concert-this": _ => {
+    !userQuery ? console.log("Who do you want to see perform?") : concertThis(userQuery);
+  },
+  "spotify-this-song": _ => {
+    !userInput ? spotifyThis("Sanctuary Joji") : spotifyThis(userQuery);
+  },
+  "movie-this":_ => {
+    !userInput ? movieThis("Parasite") : movieThis(userQuery);
+  },
+  "do-what-it-says": _ =>{
+    doThis();
+  }
+}
+
+choice[method]();
 // OMDB
 // axios
 //   .get("https://www.omdbapi.com/?t=" +query+ "&apikey=trilogy")
